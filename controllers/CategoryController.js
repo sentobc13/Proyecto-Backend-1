35c1749fea7a5067b1b873803b2f8a41f79a4f53
sentobc13 <sentoberlanga@gmail.com>
2024-05-03 13:17:15 +0200

    getAll categories

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -1,15 +1,25 @@
-const { Category } = require('../models/index');
+const { Category, Product } = require('../models/index');
+
 
 const CategoryController = {
     async insert(req, res) {
         try {
             const category = await Category.create(req.body);
-            res.status(201).send({msg:"Categoría creada con éxito",category})
+            res.status(201).send({ msg: "Categoría creada con éxito", category })
         } catch (err) {
             console.error(err);
-            res.status(500).send('Error interno del servidor');
+            res.status(500).send({ msg: 'Error interno del servidor', err });
         }
     },
+    async getAll(req, res) {
+        try {
+            const categories = await Category.findAll();
+            res.send(categories);
+        } catch (error) {
+            console.error(error);
+        }
+    },
+
 };
 
 module.exports = CategoryController;
--- routes/categories.js
+++ routes/categories.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const CategoryController = require('../controllers/CategoryController')
 
-router.post('/',CategoryController.insert)
+router.post('/',CategoryController.insert),
+router.get('/',CategoryController.getAll)
 
 module.exports = router;
