8ff74ebc0b9df345a0971e853c6091f59eb86d69
sentobc13 <sentoberlanga@gmail.com>
2024-05-07 15:37:55 +0200

    eliminar,actualizar y crear productos con authorization

--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const ProductController = require('../controllers/ProductController');
+const { authentication } = require('../middleware/authentication');
 
 
-router.post('/', ProductController.create);
-router.put('/id/:id', ProductController.update),
-router.delete('/id/:id',ProductController.delete),
+router.post('/',authentication, ProductController.create);
+router.put('/id/:id',authentication, ProductController.update),
+router.delete('/id/:id',authentication,ProductController.delete),
 router.get('/', ProductController.getAll),
 router.get('/id/:id',ProductController.getById),
 router.get('/name/:name',ProductController.getByName),
